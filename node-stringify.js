b7645a822e43d52ff473ab4e3e07a876fa845800
fuqcool <fuqcool@gmail.com>
2014-07-10 11:18:14 -0400

    less brackets

--- node-stringify.js
+++ node-stringify.js
@@ -1,33 +1,29 @@
 var _ = require('underscore')
 
 var stringify = function (obj) {
-  if (_.isNull(obj)) { return 'null' }
-  if (_.isUndefined(obj)) { return 'undefined' }
-  if (_.isRegExp(obj) || _.isNumber(obj) || _.isBoolean(obj)) {
+  if (_.isNull(obj)) return 'null'
+  if (_.isUndefined(obj)) return 'undefined'
+  if (_.isRegExp(obj) || _.isNumber(obj) || _.isBoolean(obj))
     return obj.toString()
-  }
 
-  if (_.isFunction(obj)) {
+  if (_.isFunction(obj))
     return '(' + obj.toString() + ')'
-  }
 
-  if (_.isString(obj)) {
+  if (_.isString(obj))
     return "'" + obj.replace(/'/g, "\\'").replace(/\n/g, '\\n') + "'"
-  }
 
-  if (_.isDate(obj)) { return 'new Date(' + obj.getTime() + ')' }
+  if (_.isDate(obj)) return 'new Date(' + obj.getTime() + ')'
 
-  if (_.isArguments(obj)) {
+  if (_.isArguments(obj))
     obj = _.toArray(obj)
-  }
 
-  if (_.isArray(obj)) {
+  if (_.isArray(obj))
     return '[' + _.map(obj, stringify).join(',') + ']'
-  }
 
-  if (_.isObject(obj)) {
-    return '({' + _.map(obj, function (v, k) { return k + ':' + stringify(v) }).join(',') + '})'
-  }
+  if (_.isObject(obj))
+    return '({' + _.map(obj, function (v, k) {
+      return k + ':' + stringify(v)
+    }).join(',') + '})'
 }
 
 module.exports = stringify
